62d42bb9f7564b7f99518ecb78d6b93943c5e040
Pattabhi A <pattabhi.amperayani@gmail.com>
2025-03-11 10:00:00 +0530

    --push

--- src/main/java/com/devworld/ResouceNotFoundException.java
+++ src/main/java/com/devworld/ResouceNotFoundException.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResouceNotFoundException extends RuntimeException {
+
     public ResouceNotFoundException(String message) {
         super(message);
     }
